5814c87700dedd3e497a23c881449f81f636aec6
Nicholas Guilbeault <nicholas.guilbeault@mail.utoronto.ca>
2018-06-18 02:01:51 -0400

    Added an example of how to use the track_video function.

--- free_swimming_tail_tracking_CL.py
+++ free_swimming_tail_tracking_CL.py
@@ -2,10 +2,7 @@ import free_swimming_tail_tracking
 import numpy as np
 
 # Create the video path.
-video_path = "C:\\Users\\Nicholas\\Desktop\\14-30-53.650.avi"
-
-#Create the background path.
-background_path = "C:\\Users\\Nicholas\\Desktop\\14-30-53.650_brightest_background.tif"
+video_path = "C:\\Users\\User1\\Desktop\\Video1.avi"
 
 # Set the colours to be used for drawing values of interest on the final tracked videos.
 colours =    [  (0, 0, 255),     (0, 127, 255),  (0, 255, 255),  (0, 255, 127),  (0, 255, 0),
@@ -34,4 +31,4 @@ line_length = 20
 # Set the eye threshold.
 eye_threshold = 75
 
-free_swimming_tail_tracking.track_video(video_path, colours, n_tail_points, range_tail_angles, radius_tail_points, radius_eyes, radius_swim_bladder, line_length, eye_threshold, background_path)
+free_swimming_tail_tracking.track_video(video_path, colours, n_tail_points, range_tail_angles, radius_tail_points, radius_eyes, radius_swim_bladder, line_length, eye_threshold)
