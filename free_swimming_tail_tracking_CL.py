fb4759f2237c8e17ad5e36e1c93229619ec9e0fc
Nicholas <nicholas.guilbeault@mail.utoronto.ca>
2018-09-04 12:34:28 -0400

    Added example of including custom line length into tracking parameters.

--- free_swimming_tail_tracking_CL.py
+++ free_swimming_tail_tracking_CL.py
@@ -20,17 +20,20 @@ dist_tail_points = 5
 dist_eyes = 4
 # Set the distance between the eyes and the swim bladder.
 dist_swim_bladder = 12
-# Set the threshold at which to determine whether or not to process the frame (there must be a pixel in the frame that is greater than the threshold in order for the algorithm to process the frame)
-pixel_threshold = 80
+# Set the threshold at which to determine whether or not to process the frame (there must be a pixel in the frame that is greater than the threshold in order for the algorithm to process the frame).
+pixel_threshold = 40
+
+# Set the line length that will be used for drawing the heading angle and eye angles.
+line_length = 4
 
 # Preview the results of the tracking parameters.
-tr.preview_tracking_results(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold)
+tr.preview_tracking_results(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold, line_length = line_length)
 
 # Track the video. This function will perform tail tracking, annotate the results onto video, and save the results to a .npz file.
-tr.track_video(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold)
+tr.track_video(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold, line_length = line_length)
 
 # Track the video using multiprocessing and return the results.
-results = tr.track_tail_in_video_with_multiprocessing(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold)
+results = tr.track_tail_in_video_with_multiprocessing(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold, line_length = line_length)
 
 # Track the video without using multiprocessing.
-results = tr.track_tail_in_video_without_multiprocessing(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold)
+results = tr.track_tail_in_video_without_multiprocessing(video_path, colours, n_tail_points, dist_tail_points, dist_eyes, dist_swim_bladder, background_path = background_path, pixel_threshold = pixel_threshold, line_length = line_length)
